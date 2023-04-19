88f7b951d38e42ecdf5358fb4054295111c89773
Valeriya <valeriyalukovkina@gmail.com>
2023-04-19 11:03:43 +0300

    add card film

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,18 @@
-import styles from './Main.module.css';
+import CardFilm from '../../components/cardFilm/cardFilm';
+import styles from './Main.module.scss';
+import example from './../../assests/example.jpg';
 
 const Main = () => {
   return (
     <div>
       <h2>MainPage</h2>
+      <CardFilm
+        image={example}
+        name="qwertyqqqqqqqfrgjhkl;jhvghbjnkml,"
+        year={2022}
+        country="qwerty"
+        genre="qwerty"
+      />
     </div>
   );
 };
