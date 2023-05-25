543c88e55417cae0ef2d61165fda4fd82f7b4ce3
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-05-25 20:20:10 +0300

    finish test for main and mainPageSlider

--- src/components/mainPageSlider/mainPageSlider.test.tsx
+++ src/components/mainPageSlider/mainPageSlider.test.tsx
@@ -1,11 +1,22 @@
+import 'whatwg-fetch';
 import { render, screen, act } from '@testing-library/react';
 import MainPageSlider from './MainPageSlider';
 import { BrowserRouter } from "react-router-dom";
 import { store } from '../../store/mainStore';
 import { Provider } from 'react-redux';
 import '@testing-library/jest-dom';
+import fetchMock from 'jest-fetch-mock';
+import { URL } from '../../service/constans';
 
 describe('Тестим компонент слайдера на главной странице', () => {
+  beforeAll(() => {
+    fetchMock.mockOnceIf(URL, () =>
+      Promise.resolve({
+        status: 200,
+        body: JSON.stringify([]),
+      })
+    );
+  });
   const testTitle = 'Test title';
   it('Рендерим слайдер с мелкого размера', async () => {
     await act(async () => render(<BrowserRouter><Provider store={store}><MainPageSlider genre={'test'} size={'medium'} title={testTitle} /></Provider></BrowserRouter>))
--- src/pages/main/main.test.tsx
+++ src/pages/main/main.test.tsx
@@ -1,20 +1,33 @@
+
+import 'whatwg-fetch';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Main from './Main';
 import { Provider } from 'react-redux';
 import { store } from '../../store/mainStore';
+import { URL } from '../../service/constans';
+import fetchMock from 'jest-fetch-mock';
+import { useTranslation } from 'react-i18next';
+jest.mock('react-i18next', () => ({
+  useTranslation: jest.fn()
+}));
 
-it('Главная страница рендерится без ошибок', () => {
-  jest.spyOn(Storage.prototype, 'getItem');
-  localStorage.getItem = jest.fn();
+describe('Тестим главную страницу', () => {
+  beforeEach(() => {
+    (useTranslation as jest.Mock).mockReturnValue({ t: (key: string) => key });
+  });
 
-  const tree = renderer.create(
-    <Provider store={store}>
-      <Main />
-    </Provider>
-  );
+  it('Главная страница рендерится без ошибок', () => {
+    jest.spyOn(Storage.prototype, 'getItem');
+    localStorage.getItem = jest.fn();
 
-  expect(tree).toMatchInlineSnapshot(`
+    const tree = renderer.create(
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+
+    expect(tree).toMatchInlineSnapshot(`
     <div>
       <div
         className="{spinnerStyle.spinner} undefined"
@@ -25,4 +38,5 @@ it('Главная страница рендерится без ошибок', (
       </div>
     </div>
   `);
-});
+  });
+})
