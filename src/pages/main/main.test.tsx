3ad504d71138801939f42460c9d4d1cff1536244
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-05-25 21:00:37 +0300

    add: timers in tests

--- src/pages/main/main.test.tsx
+++ src/pages/main/main.test.tsx
@@ -11,15 +11,20 @@ import { useTranslation } from 'react-i18next';
 jest.mock('react-i18next', () => ({
   useTranslation: jest.fn()
 }));
-
+jest.useFakeTimers();
 describe('Тестим главную страницу', () => {
   beforeEach(() => {
     (useTranslation as jest.Mock).mockReturnValue({ t: (key: string) => key });
   });
 
+  afterAll(() => {
+    jest.runAllTimers()
+  })
+
   it('Главная страница рендерится без ошибок', () => {
     jest.spyOn(Storage.prototype, 'getItem');
     localStorage.getItem = jest.fn();
+    jest.runAllTimers();
 
     const tree = renderer.create(
       <Provider store={store}>
