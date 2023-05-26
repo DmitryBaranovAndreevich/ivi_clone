0e9fce92850335df67b82d4966b751521b102342
Dmitry Baranov <baranow.dmitry@gmail.com>
2023-05-27 01:44:02 +0300

    update test

--- src/components/mainSlider/mainSlider.test.tsx
+++ src/components/mainSlider/mainSlider.test.tsx
@@ -1,10 +1,22 @@
+import 'whatwg-fetch';
 import { BrowserRouter } from "react-router-dom";
 import { store } from '../../store/mainStore';
 import { Provider } from 'react-redux';
 import '@testing-library/jest-dom';
 import MainSlider from './MainSlider';
 import renderer from 'react-test-renderer';
+import { URL } from '../../service/constans';
+import fetchMock from 'jest-fetch-mock';
+
 describe('Тестим компонент слайдера большого размера', () => {
+  beforeAll(() => {
+    fetchMock.mockOnceIf(URL, () =>
+      Promise.resolve({
+        status: 200,
+        body: JSON.stringify([]),
+      })
+    );
+  });
 
   it('Тест слайдера', async () => {
     const tree = renderer.create(<BrowserRouter><Provider store={store}><MainSlider items={[]} /></Provider></BrowserRouter>);
